b9997959daabfa7b2c143eab3530222cadbd8ec8
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-08 07:44:22 +0300

    all

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+  getMaxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+}
