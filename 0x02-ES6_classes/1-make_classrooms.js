fdece139ffc865268746bbe5d9640c95e62e95f9
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-08 09:13:38 +0300

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,5 @@
 import ClassRoom from './0-classroom.js';
 
 export default initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  return roomSizes.map(size => new ClassRoom(size));
+  return [new ClassRoom(19), new ClassRoom(20), new classroom(34)];
 }
