ac4a61e075e214963701bf937d274fe0746f481a
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-27 01:11:48 +0300

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+/** excecute with command line**/
+//display welcome message
+console.log("Welcome to Holberton School, what is your name?");
+
+// listedn to user input
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
+  process.exit();
+});
+
+// Handling program exit
+process.on('exit', () => {
+  console.log("This important software is now closing");
+});
